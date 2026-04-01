fa1f7acf8394fc1d610942ad1bc26823e585f21b
Mukhammad <nomonhojaevm@gmail.com>
2026-04-01 11:46:07 +0900

    feat: complete MITTASK ZK

--- apps/train.ts
+++ apps/train.ts
@@ -1,10 +1,32 @@
+/*TASK-ZK:
+Shunday function yozing, u har soniyada bir marta 
+consolega 1 dan 5 gacha bolgan raqamlarni chop etsin 
+va 5 soniyadan keyin ishini toxtatsin.
+MASALAN: printNumbers()*/
+
+function printNumbers() {
+	let count = 0;
+
+	const interval = setInterval(() => {
+		count++;
+		for (let i = 1; i <= 5; i++) {
+			console.log(i);
+		}
+		console.log('');
+		if (count === 5) {
+			clearInterval(interval);
+		}
+	}, 1000);
+}
+
+printNumbers();
 /*TASK ZJ:
 import { forEach } from '../node_modules/iterall/index.d';
 Shunday function yozing, u berilgan array ichidagi
 raqamlarni qiymatini hisoblab qaytarsin.
 MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
 Yuqoridagi misolda, array nested bo'lgan holdatda ham,
-bizning function ularning yig'indisini hisoblab qaytarmoqda.*/
+bizning function ularning yig'indisini hisoblab qaytarmoqda.
 
 function reduceNestedArray(arr: any[]) {
 	let sum = 0;
@@ -14,3 +36,4 @@ function reduceNestedArray(arr: any[]) {
 	return sum;
 }
 console.log(reduceNestedArray([1, [1, 2, [4]]]));
+*/
