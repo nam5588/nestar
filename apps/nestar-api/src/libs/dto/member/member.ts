d70bb36badeb4200e01c7ce48d3c77a69d163a3e
Mukhammad <nomonhojaevm@gmail.com>
2026-04-04 16:26:48 +0900

    fix: spelling mistakes 2

--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -28,7 +28,7 @@ export class Member {
 	memberFullName?: string;
 
 	@Field(() => String)
-	memberImage?: string;
+	memberImage: string;
 
 	@Field(() => String, { nullable: true })
 	memberAddress?: string;
@@ -46,7 +46,7 @@ export class Member {
 	memberFollowers: number;
 
 	@Field(() => Int)
-	memberFollowing: number;
+	memberFollowings: number;
 
 	@Field(() => Int)
 	memberPoints: number;
--- apps/nestar-api/src/schemas/Member.model.ts
+++ apps/nestar-api/src/schemas/Member.model.ts
@@ -71,7 +71,7 @@ const MemberSchema = new Schema(
 			default: 0,
 		},
 
-		memberFollowing: {
+		memberFollowings: {
 			type: Number,
 			default: 0,
 		},
