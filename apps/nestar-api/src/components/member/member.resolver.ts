3d33e11c82f67534494f681263924e7af9fb21e3
Mukhammad <nomonhojaevm@gmail.com>
2026-04-04 15:54:38 +0900

    feat: develop login busines logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -21,11 +21,11 @@ export class MemberResolver {
 	}
 
 	@Mutation(() => Member)
-	public async login(@Args('input') input: LoginInput): Promise<string> {
+	public async login(@Args('input') input: LoginInput): Promise<Member> {
 		try {
 			console.log('Mutation: login');
 			console.log('input: ', input);
-			return this.memberService.login();
+			return this.memberService.login(input);
 		} catch (err) {
 			console.log('Error, login', err);
 			throw new InternalServerErrorException(err);
--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -22,7 +22,7 @@ export class Member {
 	@Field(() => String)
 	memberNick: string;
 
-	memberPasswor?: string;
+	memberPassword?: string;
 
 	@Field(() => String, { nullable: true })
 	memberFullName?: string;
