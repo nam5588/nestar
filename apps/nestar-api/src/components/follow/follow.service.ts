c9fd7bc98d555da0289b8a0d99b1d20a1281bf02
Mukhammad <nomonhojaevm@gmail.com>
2026-04-23 11:18:32 +0900

    feat: create subscribe graphQL api

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Model, ObjectId } from 'mongoose';
 import { Follower, Following } from '../../libs/dto/follow/follow';
 import { MemberService } from '../member/member.service';
 import { InjectModel } from '@nestjs/mongoose';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class FollowService {
@@ -10,4 +11,32 @@ export class FollowService {
 		@InjectModel('Follow') private readonly followModel: Model<Follower | Following>,
 		private memberService: MemberService,
 	) {}
+
+	public async subscribe(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+		if (followerId.toString() === followingId.toString()) {
+			throw new InternalServerErrorException(Message.SELF_SUBSCRIPTION_DENIED);
+		}
+
+		const targetMember = await this.memberService.getMember(null, followingId);
+		if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		const result = await this.registerSubscription(followerId, followingId);
+
+		await this.memberService.memberStatusEditor({ _id: followerId, targetKey: 'memberFollowings', modifier: 1 });
+		await this.memberService.memberStatusEditor({ _id: followingId, targetKey: 'memberFollowers', modifier: 1 });
+
+		return result;
+	}
+
+	private async registerSubscription(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+		try {
+			return await this.followModel.create({
+				followingId: followingId,
+				followerId: followerId,
+			});
+		} catch (err: any) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 }
