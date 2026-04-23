c40bdb30b002844f6f47aa0d4776964f4156e9fd
Mukhammad <nomonhojaevm@gmail.com>
2026-04-23 11:57:40 +0900

    feat: create getMemberFollowings and getMemberFollowers  graphQL api

--- apps/nestar-api/src/components/follow/follow.resolver.ts
+++ apps/nestar-api/src/components/follow/follow.resolver.ts
@@ -1,11 +1,13 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { FollowService } from './follow.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
-import { Follower } from '../../libs/dto/follow/follow';
+import { Follower, Followers, Followings } from '../../libs/dto/follow/follow';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 
 @Resolver()
 export class FollowResolver {
@@ -25,5 +27,29 @@ export class FollowResolver {
 		console.log('Mutation: unsubscribe');
 		const followingId = shapeIntoMongoObjectId(input);
 		return this.followService.unsubscribe(memberId, followingId);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Followings)
+	public async getMemberFollowings(
+		@Args('input') input: FollowInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Followings> {
+		console.log('Query: getMemberFollowings');
+		const { followerId } = input.search;
+		input.search.followerId = shapeIntoMongoObjectId(followerId);
+		return this.followService.getMemberFollowings(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Followers)
+	public async getMemberFollowers(
+		@Args('input') input: FollowInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Followers> {
+		console.log('Query: getMemberFollowers');
+		const { followingId } = input.search;
+		input.search.followingId = shapeIntoMongoObjectId(followingId);
+		return this.followService.getMemberFollowers(memberId, input);
 	}
 }
