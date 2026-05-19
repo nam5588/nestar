f965f67cd771ed53e23749b3aae9e42d7e8a99fd
Mukhammad <nomonhojaevm@gmail.com>
2026-05-19 17:44:20 +0900

    fix: getBoardArticles added memberData

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -65,6 +65,7 @@ export class BoardArticleService {
 			const likeInput = { memberId: memberId, likeRefId: articleId, likeGroup: LikeGroup.ARTICLE };
 			targetBoardArticle.meLiked = await this.likeService.checkLikeExistence(likeInput);
 		}
+		targetBoardArticle.memberData = await this.memberService.getMember(null, targetBoardArticle.memberId);
 		return targetBoardArticle;
 	}
 
